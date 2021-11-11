b5b73cdf9dcaa1d0f19b162995bfdf46b06cc409
Peter Salmon <pdfsalmon@gmail.com>
2021-11-10 20:17:10 -0800

    Testing

---
+++ Later-Initial/Views/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  Later-Initial
+//
+//  Created by Peter Salmon on 2021-11-10.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
